5e0a093794b4b4a62c358ea7021df9bc5f40f820
massivebird <gdrakemail@gmail.com>
2023-12-21 22:33:17 -0500

    wip: dude

--- src/lib.rs
+++ src/lib.rs
@@ -10,32 +10,17 @@ use walkdir::WalkDir;
 
 pub mod config;
 
-type ArcMutexHashmap<K, V> = Arc<Mutex<HashMap<K, V>>>;
-
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 enum ColumnType {
     GameCount,
     FileSize,
 }
 
-impl ColumnType {
-    fn get_header(&self) -> String {
-        match self {
-            Self::GameCount => "Games".to_string(),
-            Self::FileSize => "Size".to_string(),
-        }
-    }
-
-    fn get_width(&self) -> usize {
-        self.get_header().len()
-    }
-}
-
 #[derive(Debug, Clone)]
 struct Column {
     header: String,
     col_type: ColumnType,
-    values: HashMap<System, u64>,
+    data: HashMap<System, u64>,
 }
 
 impl Column {
@@ -43,39 +28,31 @@ impl Column {
         Column {
             header: header.to_string(),
             col_type,
-            values: HashMap::new(),
+            data: HashMap::new(),
         }
     }
 
     fn calc_width(&self) -> usize {
-        if self.values.len() == 0 {
-            return 0;
+        let padding = 2;
+
+        if self.data.len() == 0 {
+            return self.header.len();
         }
 
         max(
             self.header.len(),
-            self.values.values().map(|v| v.to_string().len()).max().unwrap()
-        )
-    }
-
-    fn get_total(&self) -> u64 {
-        self.values.values().sum()
+            self.data.values().map(|v| v.to_string().len()).max().unwrap()
+        ) + padding
     }
     
-    fn add(&mut self, system: System, value: u64) {
-        self.values.entry(system.clone()).and_modify(|v| *v += value);
+    fn add(&mut self, system: &System, value: u64) {
+        self.data.entry(system.clone()).and_modify(|v| *v += value);
     }
 }
 
-fn calc_width(col_type: &ColumnType, values: ArcMutexHashmap<System, u64>) -> usize {
-    max(
-        col_type.get_width(),
-        values.lock().unwrap().values().map(|v| v.to_string().len()).max().unwrap()
-    )
-}
-
-fn bytes_to_gigabytes(bytes: u64) -> f32 {
-    bytes as f32 / 1_000_000_000.0
+fn display_bytes_as_gigabytes(bytes: u64) -> String {
+    let gb = bytes as f32 / 1_000_000_000.0;
+    format!("{gb:.2}GB")
 }
 
 fn create_thread(
@@ -91,14 +68,14 @@ fn create_thread(
                 .skip(1) // skip archive root entry
         };
 
-        // let add_if_exists = |col_type: ColumnType, value: u64| {
-        //     columns.lock().unwrap().iter_mut().find(|h| h.col_type == col_type).unwrap().add(system.clone(), value);
-        // };
+        let add_if_exists = |col_type: ColumnType, value: u64| {
+            columns.lock().unwrap().iter_mut().find(|h| h.col_type == col_type).unwrap().add(&system, value);
+        };
 
         for entry in walk_archive() {
             let file_size = entry.metadata().unwrap().len();
 
-            columns.lock().unwrap().iter_mut().find(|h| h.col_type == ColumnType::FileSize).unwrap().add(system.clone(), file_size);
+            add_if_exists(ColumnType::FileSize, file_size);
 
             // if games are represented as directories,
             // increment game count only once per directory
@@ -106,7 +83,7 @@ fn create_thread(
                 continue;
             }
 
-            // add_if_exists(ColumnType::GameCount, file_size);
+            add_if_exists(ColumnType::GameCount, 1);
         }
     })
 }
@@ -131,9 +108,9 @@ pub fn run() {
         v
     };
 
-    for column in columns.iter_mut() {
-        for system in systems.iter() {
-            column.values.entry(system.clone()).or_insert(0);
+    for column in &mut columns {
+        for system in &systems {
+            column.data.entry(system.clone()).or_insert(0);
         }
     }
 
@@ -147,57 +124,44 @@ pub fn run() {
         );
     }
 
-    let columns = columns.lock().unwrap();
-
     for thread in children_threads {
         thread.join().expect("Child thread has panicked");
     }
+
+    let columns = columns.lock().unwrap();
 
     let styled_header = |text: &str| -> ColoredString {
         text.underline().white()
     };
 
+    let system_col_header = "System";
+    let name_column_width = max(
+        system_col_header.len(),
+        systems.iter().map(|s| s.pretty_string.len()).max().unwrap(),
+    ) + 2;
+
     {
-        let width = systems.iter().map(|s| s.pretty_string.len()).max().unwrap() + 2;
-        let header = styled_header("System");
-        print!("{header}{}", " ".repeat(2 + width - header.len()));
+        let header = styled_header(system_col_header);
+        print!("{header}{}", " ".repeat(name_column_width - header.len()));
     }
 
     for col in columns.iter() {
-        let width = col.calc_width();
         let header = styled_header(&col.header);
         print!("{header}{}", " ".repeat(2));
     }
     println!();
 
-    dbg!(&columns);
-
-    let name_width = systems.iter().map(|s| s.pretty_string.len()).max().unwrap() + 2;
-    for system in systems.iter() {
-        print!("{: <name_width$}", system.pretty_string);
+    for system in &systems {
+        print!("{: <name_column_width$}", system.pretty_string);
         for col in columns.iter() {
-            let numero = col.values.get(system).unwrap();
+            let raw_data = col.data.get(system).unwrap();
+            let output = match col.col_type {
+                ColumnType::FileSize => display_bytes_as_gigabytes(*raw_data),
+                ColumnType::GameCount => raw_data.to_string(),
+            };
             let width = col.calc_width();
-            print!("{numero: <width$}");
+            print!("{output: <width$}");
         }
+        println!();
     }
-
-    // for system in all_systems_stats() {
-    //     let game_count = data.lock().unwrap()
-    //         .get(system.as_ref()).unwrap()
-    //         .get(&ColumnType::GameCount).unwrap().clone();
-
-    //     let file_size = data.lock().unwrap()
-    //         .get(system.as_ref()).unwrap()
-    //         .get(&ColumnType::FileSize).unwrap().clone();
-
-    //     println!("{: <col_1_width$}{game_count: <col_2_width$}{:.2}G",
-    //     system.pretty_string,
-    //     bytes_to_gigabytes(file_size));
-    // }
-
-    // println!("{: <col_1_width$}{: <col_2_width$}{:.2}G", " ",
-    // totals.0,
-    // bytes_to_gigabytes(totals.1),
-// );
 }
