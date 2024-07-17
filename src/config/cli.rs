a4a627a5f854c49a27c209a7ce6d363187f4343d
massivebird <gdrakemail@gmail.com>
2024-07-16 20:29:25 -0400

    removed: lib.rs, dissolved into main.rs
    I'm fixing my old lib.rs habit. This crate definitely doesn't need one
    since it isn't a library.

--- src/config/cli.rs
+++ src/config/cli.rs
@@ -1,29 +1,29 @@
-use clap::{Arg, ValueHint, Command, command};
+use clap::{command, Arg, Command, ValueHint};
 
 pub fn build_args() -> Command {
-    let desired_systems_long_help = "\
-        Query specific systems via comma-seperated system labels.\
-";
-
-    let archive_root_long_help = "\
-        Provide the path to your archive root, overriding the VG_ARCHIVE environment variable if it exists.\
-";
-
     command!()
-        .arg(Arg::new("desired_systems")
-            .short('s')
-            .long("systems")
-            .help("Comma-separated system labels to analyze exclusively")
-            .long_help(desired_systems_long_help)
-            .value_name("labels")
+        .arg(
+            Arg::new("desired_systems")
+                .short('s')
+                .long("systems")
+                .help("Comma-separated system labels to analyze exclusively")
+                .long_help(
+                    "\
+        Query specific systems via comma-seperated system labels.\
+",
+                )
+                .value_name("labels"),
         )
-        .arg(Arg::new("archive_root")
-            .short('r')
-            .long("archive-root")
-            .alias("archive-path")
-            .help("The root of your game archive")
-            .long_help(archive_root_long_help)
-            .value_name("PATH")
-            .value_hint(ValueHint::DirPath)
+        .arg(
+            Arg::new("archive_root")
+                .short('r')
+                .long("archive-root")
+                .alias("archive-path")
+                .help("The root of your game archive")
+                .long_help("\
+        Provide the path to your archive root, overriding the VG_ARCHIVE environment variable if it exists.\
+")
+                .value_name("PATH")
+                .value_hint(ValueHint::DirPath),
         )
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -8,7 +8,8 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new() -> Self {
+    /// Generates configuration options based on command line arguments.
+    pub fn generate() -> Self {
         let matches = cli::build_args().get_matches();
 
         let archive_root: String = matches.get_one::<String>("archive_root").map_or_else(
@@ -18,9 +19,13 @@ impl Config {
             String::to_string
         );
 
-        let desired_systems: Option<Vec<String>> = matches.get_one::<String>("desired_systems").map(
-            |labels| labels.split(&[',', ' '][..]).map(ToString::to_string).collect()
-        );
+        let desired_systems: Option<Vec<String>> =
+            matches.get_one::<String>("desired_systems").map(|labels| {
+                labels
+                    .split(&[',', ' '][..])
+                    .map(ToString::to_string)
+                    .collect()
+            });
 
         Self {
             archive_root,
