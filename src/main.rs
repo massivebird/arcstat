f0a46deaa17c92024a4a55e1cfef503f30ef8bad
massivebird <33991373+massivebird@users.noreply.github.com>
2025-03-29 06:48:11 -0400

    wip: coloredstring doesn't work with tabled

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 use self::config::Config;
 use arcconfig::{read_config, system::System};
-use colored::{ColoredString, Colorize};
-use rayon::prelude::*;
-use std::{
-    cmp::max,
-    collections::HashMap,
-    fs::DirEntry,
-    path::Path,
-    sync::{Arc, Mutex},
-    thread,
-};
+use colored::ColoredString;
+use tabled::settings::Style;
+use std::{collections::VecDeque, path::Path};
+use tabled::{Table, Tabled};
+use tokio::spawn;
 
 mod config;
 
-type ArcMutexHashmap<K, V> = Arc<Mutex<HashMap<K, V>>>;
+#[derive(Default)]
+struct Analysis {
+    num_games: u32,
+    file_size: u64,
+}
 
-fn main() {
+#[derive(Tabled)]
+struct TableRow {
+    #[tabled(rename = "System")]
+    system_str: String,
+    #[tabled(rename = "Num games")]
+    num_games: u32,
+    #[tabled(rename = "Size")]
+    file_size: u64,
+}
+
+#[tokio::main]
+async fn main() {
     let config = Config::generate();
 
     let systems: Vec<System> = read_config(&config.archive_root)
@@ -28,104 +38,34 @@ fn main() {
         })
         .collect();
 
-    // Record (game_count, bytes) per system.
-    let systems_stats: ArcMutexHashmap<System, (u32, u64)> = Arc::new(Mutex::new(HashMap::new()));
+    let mut handles = VecDeque::new();
 
-    thread::scope(|scope| {
-        for system_ref in &systems {
-            let systems_stats = Arc::clone(&systems_stats);
-
-            scope.spawn({
-                let config_ref = &config; // Borrow without moving config.
-                move || {
-                    analyze_system(config_ref, system_ref, &systems_stats);
-                }
-            });
-        }
-    });
-
-    let headers = ("System", "Games", "Size");
-
-    let (col_1_width, col_2_width) = {
-        let col_1 = systems.iter().map(|s| s.pretty_string.len()).max().unwrap();
-
-        let col_2 = systems_stats
-            .lock()
-            .unwrap()
-            .values()
-            .map(|(game_count, _)| game_count.to_string().len())
-            .max()
-            .unwrap();
-
-        let padding = 2;
-
-        // column space must be no less than length of header
-        (
-            max(col_1, headers.0.len()) + padding,
-            max(col_2, headers.1.len()) + padding,
-        )
-    };
-
-    let styled_header = |text: &str| -> ColoredString { text.underline().white() };
-
-    // Print header row.
-    println!(
-        "{: <col_1_width$}{: <col_2_width$}{}",
-        styled_header(headers.0),
-        styled_header(headers.1),
-        styled_header(headers.2)
-    );
-
-    // Iterates over all systems, or only user-specified systems if any.
-    for system in systems
-        .iter()
-        .filter(|s| systems_stats.lock().unwrap().contains_key(s))
-    {
-        let (game_count, file_size) = *systems_stats.lock().unwrap().get(system).unwrap();
-
-        println!(
-            "{: <col_1_width$}{game_count: <col_2_width$}{:.2}G",
-            system.pretty_string,
-            bytes_to_gigabytes(file_size)
-        );
+    for system in systems.clone() {
+        let config = config.clone();
+        handles.push_back(spawn(async { analyze_system(config, system) }));
     }
 
-    let (total_game_count, total_file_size) = systems_stats
-        .lock()
-        .unwrap()
-        .iter()
-        .fold((0, 0), |acc, (_, (game_count, file_size))| {
-            (acc.0 + game_count, acc.1 + file_size)
-        });
+    let mut table_rows: Vec<TableRow> = Vec::new();
 
-    // Prints totals row.
-    println!(
-        "{: <col_1_width$}{: <col_2_width$}{:.2}G",
-        " ",
-        total_game_count,
-        bytes_to_gigabytes(total_file_size),
-    );
+    for system in systems {
+        let analysis = handles.pop_front().unwrap().await.unwrap();
+
+        table_rows.push(TableRow {
+            system_str: system.label,
+            num_games: analysis.num_games,
+            file_size: analysis.file_size,
+        });
+    }
+
+    let mut table = Table::new(table_rows);
+
+    table.with(Style::psql());
+
+    println!("{table}");
 }
 
-fn analyze_system(
-    config: &Config,
-    system: &System,
-    systems_map: &ArcMutexHashmap<System, (u32, u64)>,
-) {
-    // Initialize this system's data.
-    systems_map
-        .lock()
-        .unwrap()
-        .entry((*system).clone())
-        .or_insert((0, 0));
-
-    let add_to_total_system_size = |n: u64| {
-        systems_map
-            .lock()
-            .unwrap()
-            .entry((*system).clone())
-            .and_modify(|v| v.1 += n);
-    };
+fn analyze_system(config: Config, system: System) -> Analysis {
+    let mut analysis = Analysis::default();
 
     let system_path = format!(
         "{}/{}",
@@ -134,15 +74,21 @@ fn analyze_system(
     );
 
     // Call this for each file.
-    let analyze_entry = |entry: DirEntry| {
+
+    for entry in Path::new(&system_path)
+        .read_dir()
+        .unwrap()
+        .filter_map(Result::ok)
+        .filter(|e| !e.path().to_string_lossy().contains("!bios"))
+    {
         let file_size = entry.metadata().unwrap().len();
 
-        add_to_total_system_size(file_size);
+        analysis.file_size += file_size;
 
         // If games are represented as directories,
         // prevent normal files from incrementing game count.
         if system.games_are_directories && entry.path().is_file() {
-            return;
+            continue;
         }
 
         if system.games_are_directories && entry.path().is_dir() {
@@ -153,25 +99,15 @@ fn analyze_system(
                 .filter_map(Result::ok)
             {
                 let file_size = part.metadata().unwrap().len();
-                add_to_total_system_size(file_size);
+                analysis.file_size += file_size;
             }
         }
 
         // Increment this system's game count.
-        systems_map
-            .lock()
-            .unwrap()
-            .entry((*system).clone())
-            .and_modify(|v| v.0 += 1);
-    };
+        analysis.num_games += 1;
+    }
 
-    Path::new(&system_path)
-        .read_dir()
-        .unwrap()
-        .par_bridge() // Run these in parallel with the rayon crate.
-        .filter_map(Result::ok)
-        .filter(|e| !e.path().to_string_lossy().contains("!bios"))
-        .for_each(analyze_entry);
+    analysis
 }
 
 fn bytes_to_gigabytes(bytes: u64) -> f32 {
